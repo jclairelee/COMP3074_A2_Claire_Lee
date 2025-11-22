20ce502be231e027cf3757a9d444939e511febc7
Claire Lee <jclairelee2@gmail.com>
2025-11-22 14:04:53 -0800

    Remove unused scripts folder

---
+++ App.js
@@ -0,0 +1,28 @@
+
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import MainScreen from "./screens/MainScreen";
+import AboutScreen from "./screens/AboutScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen
+          name="Main"
+          component={MainScreen}
+          options={{ title: "Currency Converter" }}
+        />
+        <Stack.Screen
+          name="About"
+          component={AboutScreen}
+          options={{ title: "About This App" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
